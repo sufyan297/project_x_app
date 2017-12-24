b250547b9c293c4180a15ee6b575b45fa8e82b89
Mohammed Sufyan <sufyan297@gmail.com>
2017-12-24 21:59:11 +0530

    Map added.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,42 @@
 import React from "react";
-import { } from "react-native";
+import { Dimensions } from "react-native";
 import { View,Text,Header, Left, Button, Icon, Body, Title, Right, Content, Container, Footer, FooterTab } from 'native-base';
+import MapView from 'react-native-maps';
 
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class Map extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          region: {
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+        };
+
+        console.log("CONSTRUcTOR INVOKED.");
+      }
+    
+      onMapTap(e) {
+          console.log("EVENT: ",e);
+          alert(e);
+      }
+
+      onMarkerPress(e) {
+          console.log("MARKER PRESSED!");
+        alert("MARKER"+e);
+      }
     render() {   
         return (
             <Container>
@@ -18,11 +51,28 @@ export default class Map extends React.Component {
                 </Body>
                 <Right />
                 </Header>
-                <Content>
-                <Text>
-                    This is Content Section
-                </Text>
-                </Content>
+
+                <MapView
+                    initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                    }}
+                    style={{flex:1}}
+                    onPress={e => console.log(e.nativeEvent)}
+
+                    onMarkerPress={() => this.onMarkerPress.bind(this)}
+                >
+                    <MapView.Marker
+                    title="This is a title"
+                    description="This is a description"
+                    coordinate={this.state.region}
+                    onPress={e => console.log(e.nativeEvent)}
+                    />
+                </MapView>
+
+
                 <Footer>
                 <FooterTab>
                     <Button full>
